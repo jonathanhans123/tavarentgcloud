3eaf0fcef2416eb9c19bb28a2f87eb3784addfc6
jonathanhans123 <83902947+jonathanhans123@users.noreply.github.com>
2024-01-11 08:40:23 +0700

    Update PenyewaController.php

--- src/app/Http/Controllers/PenyewaController.php
+++ src/app/Http/Controllers/PenyewaController.php
@@ -54,13 +54,13 @@ class PenyewaController extends Controller
         $param["penginapan"] = $penginapan;
         $param["photos"] = Storage::disk('public')->files('imagesPenginapan');
         $param["java"] = "<script>start();</script>";
-        if (Session::get("penyewa")->id){
+        if (Session::get("penyewa")->id!=null){
             $param["fav"] = (int)count(Penginap::find(Session::get("penyewa")->id)
         ->Penginapan()
         ->where("penginapan.id","=",$request->id)
         ->get());
         }
-        
+
         return view("penyewa.penginapan",$param);
     }
     public function ToggleFavorit(Request $request)
